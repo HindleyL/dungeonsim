bc15ad61e3f942368894a69642f991fa11b2ade6
HIN16000832 <45455801+HindleyL@users.noreply.github.com>
2019-11-20 13:39:22 +0000

    teleport and enemy

---
+++ Dungeon Simulator/Assets/Scripts/Transport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour
+{
+    public Transform waypoint;
+    CharacterController character;
+    static float nextMove;
+
+    void Start(){
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if(player){
+            character = player.GetComponent<CharacterController>();
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && Time.time > nextMove){
+            nextMove = Time.time + 5.0f;
+            character.enabled = false;
+            other.gameObject.transform.position = waypoint.position;
+            character.enabled = true;
+        }
+    } 
+}
