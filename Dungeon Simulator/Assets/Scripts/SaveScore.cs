ea16d8169e8ea4119252a0f9c4c0c0a44d8144c0
HIN16000832 <45455801+HindleyL@users.noreply.github.com>
2019-11-26 15:33:46 +0000

    Score

---
+++ Dungeon Simulator/Assets/Scripts/SaveScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            SaveTotalScore();
+            Debug.Log("Save Score");
+        }
+    }
+
+    private void SaveTotalScore()
+    {        
+        PlayerPrefs.SetInt("totalScore", UIScript.score);
+    }
+        public int GetTotalScore()
+    {
+        return PlayerPrefs.GetInt("totalScore");
+    }
+}
